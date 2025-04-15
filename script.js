42b2151e0efbc6c9debe546560680ba8352ff23b
Alvaro Buitrago <AlvaroBuitrago@users.noreply.github.com>
2025-04-15 20:49:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+ document.addEventListener("DOMContentLoaded", () => {
+    let contador = 0; // Variable para contar los clics
 
+    // Obtenemos el botón y el párrafo donde se mostrará el contador
+    const boton = document.getElementById("contadorBoton");
+    const texto = document.getElementById("contadorTexto");
+
+    // Agregamos un evento al botón para incrementar el contador
+    boton.addEventListener("click", () => {
+        contador++;
+        texto.textContent = `Clics: ${contador}`;
+    });
+});
